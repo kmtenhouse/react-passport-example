2c013d9f8a2a1f44910cf25c9e6f18895eedfd77
Tassa Tenhouse <kmtenhouse@gmail.com>
2020-01-09 01:00:44 -0800

    Hooked up signup and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,63 @@
 import React from "react";
+import API from "../utils/API";
 
-function Login() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h1 className="display-4">Login</h1>
-                    <p className="lead">Please log in to continue.</p>
-                    <hr className="my-4" />
-                    <p>It uses utility classNames for typography and spacing to space content out within the larger container.</p>
+class Login extends React.Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+    handleChange = event => {
+        event.preventDefault();
+
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+
+        // Updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const { email, password } = this.state; //grab the current state for email and password
+
+        API.login(email, password)
+            .then(result => {
+                console.log(result);
+                this.setState({ email: '', password: '' });
+            })
+            .catch(err => console.log(err.message));
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 card">
+                        <div className="card-body">
+                            <h5 className="card-title">Sign Up</h5>
+                            <p className="card-text">Please sign up for an account.</p>
+                            <form onSubmit={this.handleSubmit}> 
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Login;
